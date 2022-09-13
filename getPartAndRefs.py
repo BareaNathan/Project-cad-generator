dcb321304040c8afa42743a5932c3a2bf2056ced
BareaNathan <82299763+BareaNathan@users.noreply.github.com>
2022-09-12 21:44:47 -0300

    Top + Bot Layer
    Including Top and Bot Layer for both CAD file format, also a visual adjustment

--- getPartAndRefs.py
+++ getPartAndRefs.py
@@ -8,17 +8,17 @@ def get_spreadsheet(name):
 	return sh
 
 #Function to get the columns of references and parnumber from XLS
-def get_columns(sheet):
-	col_b = sheet.col(1)
-	col_b.insert(0," ")
-	str_col_b = str(col_b)
-	list_col_b = str_col_b.split(', ')
+def get_columns(sheet,part_col,ref_col):
+	part = sheet.col(part_col)
+	part.insert(0," ")
+	str_part = str(part)
+	list_part = str_part.split(', ')
 
-	col_e = sheet.col(4)
-	str_col_e = str(sheet.col(4))
-	list_col_e = str_col_e.split(',')
+	ref = sheet.col(ref_col)
+	str_ref = str(sheet.col(ref_col))
+	list_ref = str_ref.split(',')
 
-	return list_col_e, list_col_b
+	return list_ref, list_part
 
 #Function to treat the list, getting just the important information
 def clean_lists(ref_list, part_list):
@@ -65,13 +65,17 @@ def get_csv_data(name, template_model):
 
 #Unificate it all
 def join_csv_and_xls(csv_data,xls_ref_data,xls_part_data):
-	for row in csv_data:
+	temp_csv = csv_data.copy()
+	for row in temp_csv:
 		if row[0] in xls_ref_data:
 			xls_ref_index = xls_ref_data.index(row[0])
 			row.append(xls_part_data[xls_ref_index])
+		
 		else:
 			row.append('Not found in XLS file')
-	return csv_data
+		
+
+	return temp_csv
 
 #Write a new CSV file with the output data
 def output_csv(filename, list_of_contents):
--- main_gui.py
+++ main_gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import copy
 from tkinter import ttk
 from getPartAndRefs import *
 from getFileList import *
@@ -10,24 +11,48 @@ def main_function(chosen_xls,chosen_csv):
 	index_for_csv = csv_files_name.index(chosen_csv)
 	path_for_csv = csv_files[index_for_csv]
 
-	write_output_file(path_for_xls,path_for_csv)
+	call_write_function(path_for_xls,path_for_csv)
 	
-
-def write_output_file(path_xls,path_csv):
+def call_write_function(path_xls,path_csv):
 	#Calling the Part and Refs functions
 	sheet_name = get_spreadsheet(path_xls)	#xls_files[40] 
-	ref_list, part_list = get_columns(sheet_name)
-	ref, part = clean_lists(ref_list, part_list)
 
 	cad_type = CAD.get()
 
 	csv_data = get_csv_data(path_csv,cad_type)	#csv_files[80]
-	joined_data = join_csv_and_xls(csv_data,ref,part)
 
 
+	board_layer_conf = layer.get()
+
+	if board_layer_conf == 'Single':
+		ref_list, part_list = get_columns(sheet_name,1,4)
+		ref, part = clean_lists(ref_list, part_list)
+		joined_data = join_csv_and_xls(csv_data,ref,part)
+		write_output_file(path_xls,joined_data,'FUJI.csv')
+
+	elif board_layer_conf == 'TOP+BOT':
+		temp_csv_top = copy.deepcopy(csv_data)
+		print(temp_csv_top)
+
+		ref_list, part_list = get_columns(sheet_name,1,5)
+		ref, part = clean_lists(ref_list, part_list)
+		joined_data = join_csv_and_xls(temp_csv_top,ref,part)
+		write_output_file(path_xls,joined_data,'FUJI.csv')
+
+		temp_csv_bot = copy.deepcopy(csv_data)
+		print(temp_csv_bot)
+		ref_list_bot, part_list_bot = get_columns(sheet_name,3,6)
+		ref_bot, part_bot = clean_lists(ref_list_bot, part_list_bot)
+		joined_data_bot = join_csv_and_xls(temp_csv_bot,ref_bot,part_bot)
+		write_output_file(path_xls,joined_data_bot,'FUJI-BOT.csv')
+
+
+
+def write_output_file(path_xls,joined_data,sulfix):
+	
 	#Edit Path and name to output CSV file
 	ref_file_path = path_xls	#xls_files[40]
-	new_file_path = ref_file_path[:-4] + '-FUJI.csv'
+	new_file_path = ref_file_path[:-4] + '-' + sulfix
 	print(new_file_path)
 
 	#Output data
@@ -60,30 +85,45 @@ root.columnconfigure(index=1,weight=1)
 
 #Frames and Widgets
 top_cont = tk.Frame(root)
-top_cont_label_title = tk.Label(top_cont,text='Label').grid()
+top_cont_label_title = tk.Label(top_cont,text='CSV Generator 1.2').grid()
 
 
 mid_cont_1 = tk.Frame(root)
-mid_cont_1_label_title = tk.Label(mid_cont_1,text='Label').grid()
+mid_cont_1_label_title = tk.Label(mid_cont_1,text='XLS').grid()
 mid_cont_1_listbox = ttk.Combobox(
 	mid_cont_1,
 	state = 'readonly',
 	width = 25,
 	values = xls_files_name_sorted
 	)
-mid_cont_1_listbox.set('texto')
+mid_cont_1_listbox.set('Selecione o Arquivo...')
 mid_cont_1_listbox.grid()
 
+layer = tk.StringVar(mid_cont_1,value='',name = 'layer')
+
+mid_cont_1_checkbutton_1 = ttk.Radiobutton(
+	mid_cont_1,
+	text = 'Single Layer Board',
+	variable = 'layer',
+	value = 'Single'
+	).grid()
+
+mid_cont_1_checkbutton_3 = ttk.Radiobutton(
+	mid_cont_1,
+	text = 'TOP+BOT Layer Board',
+	variable = 'layer',
+	value = 'TOP+BOT'
+	).grid()
 
 mid_cont_2 = tk.Frame(root)
-mid_cont_2_label_title = tk.Label(mid_cont_2,text='Label').grid()
+mid_cont_2_label_title = tk.Label(mid_cont_2,text='CAD').grid()
 mid_cont_2_listbox = ttk.Combobox(
 	mid_cont_2,
 	state = 'readonly',
 	width = 50,
 	values = csv_files_name_sorted
 	)
-mid_cont_2_listbox.set('texto')
+mid_cont_2_listbox.set('Selecione o Arquivo...')
 mid_cont_2_listbox.grid()
 
 CAD = tk.StringVar(mid_cont_2,value="",name = 'CAD')
@@ -102,16 +142,17 @@ mid_cont_2_checkbutton_2 = ttk.Radiobutton(
 	value = 'Protel'
 	).grid()
 
+
 bot_cont = tk.Frame(root)
-bot_cont_label_title = tk.Label(bot_cont,text='Label').grid()
+bot_cont_label_title = tk.Label(bot_cont,text='Clique em OK para gerar o(s) arquivo(s) CSV').grid()
 bot_cont_button = tk.Button(bot_cont,
-	text='Clique-me',
-	##Set these values in main.py scrpit
+	text='OK',
 	command = lambda: main_function(
 		mid_cont_1_listbox.get(),
 		mid_cont_2_listbox.get()
 		)
 	).grid()
+
 
 #Grid the containers
 top_cont.grid(row=0,columnspan=2)
